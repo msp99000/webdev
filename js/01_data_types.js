e459c9527b53b52b4354a399ce37acc4398da7fe
Manpreet Singh <msp99000@gmail.com>
2025-03-18 01:36:23 +0530

    chore: adds Objects, Arrays and Functions

--- js/01_data_types.js
+++ js/01_data_types.js
@@ -16,12 +16,13 @@ There are two type of datatypes in Javascript
         - Function
         - Date
 
-*/
+Primitive Data types are stored in Stack while the Non-Primitive/Reference Data Types are stored in Heap
 
+*/
 
 // 1. Primitive Data Types (Immutable, Stored by Value)
 
-let myName = "Hunter"; // string
+let myName = "John"; // string
 let age = 30; // number
 let isAdult = true; // boolean
 let state; // undefined
@@ -62,6 +63,18 @@ let string3 = `${string1} ${string2} World!`;
 
 console.log(`The resultant string literal is "${string3}"`);
 
+/* 
+
+Common methods in strings include
+    - .toUpperCase()
+    - .length
+    - .includes()
+    - .trim()
+    - .slice()
+    - .replace()  
+
+*/
+
 // Symbol (used for unique identifiers)
 
 let sym1 = Symbol("id");
@@ -76,5 +89,47 @@ so console.log(sym1 === sym2) will return `false`
 
 console.log(`Are sym1 and sym2 equal? (sym1 === sym2) ==> ${sym1 === sym2}`);
 
-
 // 2. Non-Primitive/Reference Data Types (mutable, stored by reference)
+
+// 2.1. Objects
+let user = {
+  userName: "John",
+  age: 30,
+  isAdmin: true,
+};
+
+console.log(`The user name is ${user.userName}`);
+console.log(`The datatype of "user" is ==> ${typeof user}`);
+
+// 2.2. Array
+let arr = [10, "foo", false]
+
+console.log(`The Array "arr" is ==> ${arr}`);
+console.log(`The first value is ==> ${arr[0]}`);
+
+/* 
+
+Common methods in arrays include
+    - push()
+    - pop()
+    - shift()
+    - unshift()
+    - map()
+    - filter()
+    - reduce()   
+
+*/
+
+// 2.3. Functions
+function greet(name) {
+    return `Hello ${name}`;
+}
+
+console.log(greet('John'));
+
+// 2.4. Date
+let nowDate = new Date();
+
+console.log(`Date right now is ==> ${nowDate.toISOString()}`);  // Current Date in ISO Format
+
+
